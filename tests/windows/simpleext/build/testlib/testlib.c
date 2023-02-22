4442102cd4d6253451af2c5347f886159b7a84c8
Jeremy Volkman <jeremy@jvolkman.com>
2023-02-22 13:11:27 -0800

    wip

--- tests/windows/simpleext/build/testlib/testlib.c
+++
@@ -1,5 +0,0 @@
-#include "testlib.h"
-
-EXPORTED int get_answer() {
-    return 42;
-}
--- tests/windows/simpleext/build/testlib/testlib.h
+++
@@ -1,7 +0,0 @@
-#ifdef _WIN32
-#  define EXPORTED __declspec( dllexport )
-#else
-#  define EXPORTED extern
-#endif
-
-EXPORTED int get_answer();
