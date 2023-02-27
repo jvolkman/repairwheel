a7fde2c71d7aff6df0897ddb331be955c203e8c5
Jeremy Volkman <jeremy@jvolkman.com>
2023-02-27 14:47:32 -0800

    infra for multi-plat testing

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,76 +1,59 @@
-import os
-import re
-import pathlib
-import subprocess
 import sys
-import tempfile
 import zipfile
+from pathlib import Path
+
+import pytest
 
 
-test_root = pathlib.Path(__file__).parent.resolve()
-lib_dir = test_root / "testwheel" / "lib"
-
-linux_wheel = test_root / "testwheel" / "testwheel-0.0.1-cp36-abi3-linux_x86_64.whl"
-macos_wheel = test_root / "testwheel" / "testwheel-0.0.1-cp36-abi3-macosx_11_0_arm64.whl"
-windows_wheel = test_root / "testwheel" / "testwheel-0.0.1-cp36-abi3-win_amd64.whl"
-
-
-def check_call(args: list):
-    return subprocess.check_call(args, env=os.environ)
+def test_wheel_contains_testdep(patched_wheel: Path) -> None:
+    with zipfile.ZipFile(patched_wheel, "r") as zf:
+        for file in zf.filelist:
+            if "testdep" in file.filename.lower():
+                break
+        else:
+            assert False, f"testdep not found in wheel: {patched_wheel}"
 
 
-def patch(wheel: pathlib.Path, out_dir: pathlib.Path) -> pathlib.Path:
-    before = list(out_dir.glob("*.whl"))
-    check_call(
-        [
-            sys.executable,
-            "-m",
-            "repairwheel",
-            "--lib-dir",
-            lib_dir,
-            wheel,
-            "--output-dir",
-            str(tmp),
-        ]
-    )
-    files = list(pathlib.Path(tmp).glob("*.whl"))
-    assert len(files) == 1, "Expected one output wheel to be generated"
-    return files[0]
+def test_wheel_installs_and_runs(patched_wheel: Path) -> None:
+    if sys.platform == "linux":
+        if "linux" not in patched_wheel.name:
+            pytest.skip(f"Wheel not installable on linux: {patched_wheel.name}")
+    elif sys.platform == "darwin":
+        if "macos" not in patched_wheel.name:
+            pytest.skip(f"Wheel not installable on macos: {patched_wheel.name}")
+    elif sys.platform == "win32":
+        if "win" not in patched_wheel.name:
+            pytest.skip(f"Wheel not installable on windows: {patched_wheel.name}")
+    else:
+        assert False, f"test cannot run on {sys.platform}"
+
+# def test_patched_testwheel_runs():
+#     """Basic repair for the testwheel package"""
+
+#     test_dir = pathlib.Path(__file__).parent
+
+#     with tempfile.TemporaryDirectory() as tmp:
+#         tmp = pathlib.Path(tmp)
+#         check_call(
+#             [
+#                 sys.executable,
+#                 "-m",
+#                 "repairwheel",
+#                 "--lib-dir",
+#                 test_dir / "testwheel" / "lib",
+#                 test_dir / "testwheel" / "testwheel-0.0.1-cp36-abi3-win_amd64.whl",
+#                 "--output-dir",
+#                 str(tmp),
+#             ]
+#         )
+#         with zipfile.ZipFile(tmp / "testwheel-0.0.1-cp36-abi3-win_amd64.whl") as wheel:
+#             for path in zipfile.Path(wheel, "testwheel.libs/").iterdir():
+#                 if path.name.startswith("testdep-"):
+#                     assert is_mangled(path.name), f"{path.name} is mangled"
+#                     break
+#             else:
+#                 assert False, "did not find testdep dll"
 
 
-@pytest.fixture
-def 
 
 
-
-@p
-def 
-
-
-
-def test_patched_testwheel_runs():
-    """Basic repair for the testwheel package"""
-
-    test_dir = pathlib.Path(__file__).parent
-
-    with tempfile.TemporaryDirectory() as tmp:
-        tmp = pathlib.Path(tmp)
-        check_call(
-            [
-                sys.executable,
-                "-m",
-                "repairwheel",
-                "--lib-dir",
-                test_dir / "testwheel" / "lib",
-                test_dir / "testwheel" / "testwheel-0.0.1-cp36-abi3-win_amd64.whl",
-                "--output-dir",
-                str(tmp),
-            ]
-        )
-        with zipfile.ZipFile(tmp / "testwheel-0.0.1-cp36-abi3-win_amd64.whl") as wheel:
-            for path in zipfile.Path(wheel, "testwheel.libs/").iterdir():
-                if path.name.startswith("testdep-"):
-                    assert is_mangled(path.name), f"{path.name} is mangled"
-                    break
-            else:
-                assert False, "did not find testdep dll"
--- tests/test_windows.py
+++ tests/test_windows.py
@@ -1,14 +1,5 @@
-import os
 import re
-import pathlib
-import subprocess
-import sys
-import tempfile
 import zipfile
-
-
-def check_call(args: list):
-    return subprocess.check_call(args, env=os.environ)
 
 
 def is_mangled(filename: str) -> bool:
@@ -16,29 +7,12 @@ def is_mangled(filename: str) -> bool:
     return re.match(r"^[^-]+-[0-9a-f]{32}\.dll$", filename.lower()) is not None
 
 
-def test_testwheel():
+def test_testwheel(patched_windows_wheel):
     """Basic repair for the testwheel package"""
-
-    test_dir = pathlib.Path(__file__).parent
-
-    with tempfile.TemporaryDirectory() as tmp:
-        tmp = pathlib.Path(tmp)
-        check_call(
-            [
-                sys.executable,
-                "-m",
-                "repairwheel",
-                "--lib-dir",
-                test_dir / "testwheel" / "lib",
-                test_dir / "testwheel" / "testwheel-0.0.1-cp36-abi3-win_amd64.whl",
-                "--output-dir",
-                str(tmp),
-            ]
-        )
-        with zipfile.ZipFile(tmp / "testwheel-0.0.1-cp36-abi3-win_amd64.whl") as wheel:
-            for path in zipfile.Path(wheel, "testwheel.libs/").iterdir():
-                if path.name.startswith("testdep-"):
-                    assert is_mangled(path.name), f"{path.name} is mangled"
-                    break
-            else:
-                assert False, "did not find testdep dll"
+    with zipfile.ZipFile(patched_windows_wheel) as wheel:
+        for path in zipfile.Path(wheel, "testwheel.libs/").iterdir():
+            if path.name.startswith("testdep-"):
+                assert is_mangled(path.name), f"{path.name} is mangled"
+                break
+        else:
+            assert False, "did not find testdep dll"
