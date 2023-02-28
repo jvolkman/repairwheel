e4bac7e5ae17d55b9faccb25fd81eeb0946975dc
Jeremy Volkman <jeremy@jvolkman.com>
2023-02-28 14:39:37 -0800

    Fix test_windows.py

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -7,9 +7,9 @@ def is_mangled(filename: str) -> bool:
     return re.match(r"^[^-]+-[0-9a-f]{32}\.dll$", filename.lower()) is not None
 
 
-def test_testwheel(patched_windows_wheel):
+def test_testwheel(patched_windows_x86_64_wheel):
     """Basic repair for the testwheel package"""
-    with zipfile.ZipFile(patched_windows_wheel) as wheel:
+    with zipfile.ZipFile(patched_windows_x86_64_wheel) as wheel:
         for path in zipfile.Path(wheel, "testwheel.libs/").iterdir():
             if path.name.startswith("testdep-"):
                 assert is_mangled(path.name), f"{path.name} is mangled"
