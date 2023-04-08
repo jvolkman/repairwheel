4665f6d2be7e86943084e0d19e11233d810101b8
Jeremy Volkman <jeremy@jvolkman.com>
2023-04-07 20:04:40 -0700

    Rename macos wheels

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,7 +68,7 @@ def patched_linux_x86_64_wheel(orig_linux_x86_64_wheel: TestWheel, patched_wheel
 
 @pytest.fixture(scope="session")
 def orig_macos_x86_64_wheel(testwheel_root: Path) -> TestWheel:
-    tag = "cp36-abi3-macosx_11_0_x86_64"
+    tag = "cp36-abi3-macosx_10_11_x86_64"
     return TestWheel(
         tag,
         testwheel_root / tag / f"testwheel-0.0.1-{tag}.whl",
@@ -83,7 +83,7 @@ def patched_macos_x86_64_wheel(orig_macos_x86_64_wheel: Path, patched_wheel_area
 
 @pytest.fixture(scope="session")
 def orig_macos_arm64_wheel(testwheel_root: Path) -> TestWheel:
-    tag = "cp36-abi3-macosx_11_0_arm64"
+    tag = "cp36-abi3-macosx_10_11_arm64"
     return TestWheel(
         tag,
         testwheel_root / tag / f"testwheel-0.0.1-{tag}.whl",
