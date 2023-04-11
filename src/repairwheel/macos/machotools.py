2aca4be6243376c2d242ce01c86a6f22cfc1c45a
Jeremy Volkman <jeremy@jvolkman.com>
2023-04-11 14:11:00 -0700

    Fix missed import

--- src/repairwheel/macos/machotools.py
+++ src/repairwheel/macos/machotools.py
@@ -371,10 +371,10 @@ def validate_signature(filename: str) -> None:
 
 
 def zip2dir(zip_fname: str, out_dir: str) -> None:
-    from auditwheel.wheeltools import zip2dir
+    from repairwheel._vendor.auditwheel.wheeltools import zip2dir
     return zip2dir(zip_fname, out_dir)
 
 
 def dir2zip(in_dir, zip_fname):
-    from auditwheel.wheeltools import dir2zip
+    from repairwheel._vendor.auditwheel.wheeltools import dir2zip
     return dir2zip(in_dir, zip_fname)
