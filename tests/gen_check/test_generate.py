081ffa913c929488be6a33c7d119fb666e42eb08
Jeremy Volkman <jeremy@jvolkman.com>
2023-04-11 16:51:35 -0700

    Make directory

--- tests/gen_check/test_generate.py
+++ tests/gen_check/test_generate.py
@@ -11,5 +11,6 @@ if "TESTWHEEL_GENERATE_PATH" not in os.environ:
 
 def test_store_patched_wheel(patched_wheel: Path) -> None:
     out_dir = Path(os.environ["TESTWHEEL_GENERATE_PATH"])
+    out_dir.mkdir(parents=True, exist_ok=True)
     out_file = out_dir / patched_wheel.name
     shutil.copy(patched_wheel, out_file)
