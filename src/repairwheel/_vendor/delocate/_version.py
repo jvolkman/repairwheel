d22b5fa8942d20767a367ed1fc71212bef689f82
Jeremy Volkman <jeremy@cedarai.com>
2023-11-24 16:45:20 -0800

    Bump delocate version

--- src/repairwheel/_vendor/delocate/_version.py
+++ src/repairwheel/_vendor/delocate/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.10.5'
-__version_tuple__ = version_tuple = (0, 10, 5)
+__version__ = version = '0.10.6'
+__version_tuple__ = version_tuple = (0, 10, 6)
